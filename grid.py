042f0838bc35f958cfaa2a8b942dfcae9feebd8d
Cobran53 <66669093+Cobran53@users.noreply.github.com>
2022-01-17 10:04:18 +0100

    ajout du module csv et d'un lecteur basique de csv, pas fini

--- grid.py
+++ grid.py
@@ -1,2 +1,18 @@
+import csv
+
+
 class Grid:
-    pass
+    def __init__(self, path):
+        with open(path, newline='') as f:
+            reader = csv.reader(f)
+            grid = [[elt.split("\\") for elt in row] for row in reader]
+            for i in grid:
+                print(i)
+
+
+def main():
+    a = Grid(r"C:\Users\nolan\OneDrive\Bureau\kakuro_ex.csv")
+
+
+if __name__ == '__main__':
+    main()
