ae5cb839535fb018bed44a7d214627a9ac6d43c0
Cobran53 <66669093+Cobran53@users.noreply.github.com>
2022-01-31 08:29:32 +0100

    Remis la classe grid

--- grid.py
+++ grid.py
@@ -31,6 +31,13 @@ root.mainloop()
 def main():
     a = Grid(r"C:\Users\nolan\OneDrive\Bureau\kakuro_ex.csv")
 
-
+class Grid:
+    def __init__(self, path):
+        with open(path, newline='') as f:
+            reader = csv.reader(f)
+            grid = [[elt.split("\\") for elt in row] for row in reader]
+            for i in grid:
+                print(i)
+    
 if __name__ == '__main__':
     main()
