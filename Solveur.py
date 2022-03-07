a9470f48f2adf67edac48bf8ba4ceb0383220476
SilvRTiger <esteban3788@gmail.com>
2022-03-07 12:08:02 +0100

    Contraintes solveur (corigé#1)

--- Solveur.py
+++ Solveur.py
@@ -4,57 +4,55 @@ prob = LpProblem("KakuroSolv", LpMinimize)
 
 # variables
 # nb max de sommes par lignes et colonnes
-K = LpVariable(3)
+K = 3
 # nb case max par ligne et colonnes
-n = LpVariable(10)
+n = 10
 
-bLa = LpVariable(("i", 0, n, "Integer"), ("k", 0, K, "Integer"))
-bLb = LpVariable(("i", 0, n, "Integer"), ("k", 0, K, "Integer"))
-bLs = LpVariable(("i", 0, n, "Integer"), ("k", 0, K, "Integer"))
-bCa = LpVariable(("j", 0, n, "Integer"), ("k", 0, K, "Integer"))
-bCb = LpVariable(("j", 0, n, "Integer"), ("k", 0, K, "Integer"))
-bCs = LpVariable(("j", 0, n, "Integer"), ("k", 0, K, "Integer"))
+bLa = ("i", 0, n, "Integer"), ("k", 0, K, "Integer")
+bLb = ("i", 0, n, "Integer"), ("k", 0, K, "Integer")
+bLs = ("i", 0, n, "Integer"), ("k", 0, K, "Integer")
+bCa = ("j", 0, n, "Integer"), ("k", 0, K, "Integer")
+bCb = ("j", 0, n, "Integer"), ("k", 0, K, "Integer")
+bCs = ("j", 0, n, "Integer"), ("k", 0, K, "Integer")
 
-x = LpVariable.dicts("x", ((range (int(n))
-                            ), (range(n)), (range(10)) , 'Binary'))
+x = LpVariable.dicts("x", ((range(int(n))), (range(n)), (range(10)), 'Binary'))
 
-#prob += (lpSum([x[for i in range(0,n), for j in range (0,n),for u in range (0,9)]]))
+# prob += (lpSum([x[for i in range(0,n), for j in range (0,n),for u in range (0,9)]]))
 
-#contraintes
+# contraintes
 
-#contr_1
-for i in range(n) :
-    for j in range (n) :
+# contr_1
+for i in range(n):
+    for j in range(n):
         prob += lpSum([x[i][j][u] for u in range(10)]) == 1
 
-#ctr0
-for i in range (n) :
-    for k in range (K):
-        for j in range (bLa[i,k], bLb[i,k]+1):
-            prob += lpSum([x[i][j][u] for u in range(10)])*bLs[i,k] >= bLs[i,k]
+# ctr0
+for i in range(n):
+    for k in range(K):
+        for j in range(bLa[i][k], bLb[i][k] + 1):
+            prob += lpSum([x[i][j][u] for u in range(10)]) * bLs[i][k] >= bLs[i][k]
 
-#ctr1
-for i in range (n) :
-    for k in range (K):
-        prob += lpSum(u*[x[i][j][u] for u in range(10) for j in range(bLa[i,k], bLb[i,k])]) == bLs[i,k]
+# ctr1
+for i in range(n):
+    for k in range(K):
+        prob += lpSum([x[i][j][u]*u for u in range(10) for j in range(bLa[i][k], bLb[i][k])]) == bLs[i][k]
 
-#ctr2
-for i in range (n) :
-    for k in range (K):
-        for u in range (10):
-            prob += lpSum(u*[x[i][j][u] for j in range(bLa[i,k], bLb[i,k])]) <= 1
+# ctr2
+for i in range(n):
+    for k in range(K):
+        for u in range(10):
+            prob += lpSum([x[i][j][u] for j in range(bLa[i][k], bLb[i][k])]) <= 1
 
-#ctr3
-for j in range (n) :
-    for k in range (K):
-        prob += lpSum(u*[x[i][j][u] for u in range(10) for i in range(bCa[i,k], bCb[i,k])]) == bCs[i,k]
+# ctr3
+for j in range(n):
+    for k in range(K):
+        prob += lpSum([x[i][j][u]*u for u in range(10) for i in range(bCa[j][k], bCb[j][k])]) == bCs[j][k]
 
-#ctr4
-for j in range (n) :
-    for k in range (K):
-        for u in range (10):
-            prob += lpSum(u*[x[i][j][u] for i in range(bCa[i,k], bCb[i,k])]) <= 1
+# ctr4
+for j in range(n):
+    for k in range(K):
+        for u in range(10):
+            prob += lpSum([x[i][j][u] for i in range(bCa[i][k], bCb[i][k])]) <= 1
 
-#solve
-
-
+# solve
+prob.solve()
