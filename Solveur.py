a79ff56a053373c4cb5953d8e4c94288dda40bcc
SilvRTiger <esteban3788@gmail.com>
2022-03-14 09:14:40 +0100

    Contraintes solveur (corigé#3)

--- Solveur.py
+++ Solveur.py
@@ -3,7 +3,9 @@ from pulp import *
 def solv(K, n, bLa, bLb, bLs, bCa, bCb, bCs):
     prob = LpProblem("KakuroSolv", LpMinimize)
 
-    x = LpVariable("x", ((range(int(n))), (range(n)), (range(10)), 'Binary'))
+    x = LpVariable.dicts("x", (range(int(n)), (range(n)), (range(1,10))), cat="Binary")
+
+
 
     # prob += (lpSum([x[for i in range(0,n), for j in range (0,n),for u in range (0,9)]]))
 
@@ -12,35 +14,48 @@ def solv(K, n, bLa, bLb, bLs, bCa, bCb, bCs):
     # contr_1
     for i in range(n):
         for j in range(n):
-            prob += lpSum([x[i][j][u] for u in range(10)]) == 1
+            prob += lpSum([x[i][j][u] for u in range(1, 10)]) == 1
 
     # ctr0
     for i in range(n):
         for k in range(K):
             for j in range(bLa[i][k], bLb[i][k] + 1):
-                prob += lpSum([x[i][j][u] for u in range(10)]) * bLs[i][k] >= bLs[i][k]
+                prob += lpSum([x[i][j][u] for u in range(1, 10)]) * bLs[i][k] >= bLs[i][k]
 
     # ctr1
     for i in range(n):
         for k in range(K):
-            prob += lpSum([x[i][j][u] * u for u in range(10) for j in range(bLa[i][k], bLb[i][k])]) == bLs[i][k]
+            prob += lpSum([x[i][j][u] * u for u in range(1,10) for j in range(bLa[i][k], bLb[i][k])]) == bLs[i][k]
 
     # ctr2
     for i in range(n):
         for k in range(K):
-            for u in range(10):
+            for u in range(1, 10):
                 prob += lpSum([x[i][j][u] for j in range(bLa[i][k], bLb[i][k])]) <= 1
 
     # ctr3
     for j in range(n):
         for k in range(K):
-            prob += lpSum([x[i][j][u] * u for u in range(10) for i in range(bCa[j][k], bCb[j][k])]) == bCs[j][k]
+            prob += lpSum([x[i][j][u] * u for u in range(1,10) for i in range(bCa[j][k], bCb[j][k])]) == bCs[j][k]
 
     # ctr4
     for j in range(n):
         for k in range(K):
-            for u in range(10):
-                prob += lpSum([x[i][j][u] for i in range(bCa[i][k], bCb[i][k])]) <= 1
+            for u in range(1, 10):
+                prob += lpSum([x[i][j][u] for i in range(bCa[j][k], bCb[j][k])]) <= 1
 
-    # solve
+    # The problem data is written to an .lp file
+    prob.writeLP("Sudoku.lp")
+
+    # The problem is solved using PuLP's choice of Solver
     prob.solve()
+
+    # The status of the solution is printed to the screen
+    print("Status:", LpStatus[prob.status])
+
+
+    for i in range(n):
+        for j in range(n):
+            for u in range(1,10):
+                if value(x[i][j][u]) == 1 :
+                    print (value(x[i][j][u]), i, j, u)
